c502c6582b7ce260aeeed45033b94c8369f9d03c
D4v1D-lab <deividfrend@gmail.com>
2020-11-25 02:29:57 -0700

    sort

--- week7/MyScriptureJournal/MyScriptureJournal/Pages/Movies/Index.cshtml.cs
+++ week7/MyScriptureJournal/MyScriptureJournal/Pages/Movies/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,9 +25,12 @@ namespace MyScriptureJournal.Pages.Movies
         public SelectList Notes { get; set; }
         [BindProperty(SupportsGet = true)]
         public string NoteBook { get; set; }
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
+  
             // Use LINQ to get list of genres.
             IQueryable<string> genreQuery = from m in _context.Scripture
                                             orderby m.Book
@@ -44,8 +48,32 @@ namespace MyScriptureJournal.Pages.Movies
             {
                 movies = movies.Where(x => x.Book == NoteBook);
             }
+            //sort
+
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            IQueryable<Scripture> studentsIQ = from s in _context.Scripture
+                                               select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.Book);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.CreatedDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.CreatedDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.Book);
+                    break;
+            }
             Notes = new SelectList(await genreQuery.Distinct().ToListAsync());
             Scripture = await movies.ToListAsync();
+            Scripture = await studentsIQ.AsNoTracking().ToListAsync();
         }
 
     }
